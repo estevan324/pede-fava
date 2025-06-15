c846ec7068ecfae6bb4302b3fa39e5998ab7698b
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2025-06-15 11:04:19 -0300

    feat: home do website

--- public/scripts/components.js
+++ public/scripts/components.js
@@ -3,3 +3,9 @@ fetch("../components/navbar.html")
   .then((html) => {
     document.getElementById("header").innerHTML = html;
   });
+
+fetch("../components/footer.html")
+  .then((res) => res.text())
+  .then((html) => {
+    document.getElementById("footer").innerHTML = html;
+  });
